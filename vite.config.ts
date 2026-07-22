b51ac63f56482b4488c0cfec4c121d52d98a0d86
eyergens <elizabeth.yergens@outlook.com>
2026-07-22 16:34:43 -0500

    add manual chunks for build and update script

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true,
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    }
   },
   resolve: {
     dedupe: ['react', 'react-dom']
